0fdb610ae88f36a6318de4add7701f057ddb38d3
Wizer27 <svd2yar@gmail.com>
2026-02-15 22:53:59 +0300

    main_init

--- bot/config.py
+++
@@ -1,8 +0,0 @@
-import aiogram
-from dotenv import load_dotenv
-import os
-from aiogram import Bot,Dispatcher
-import asyncio
-from handlers import router
-import sys
-from config import PROJECT_ROOT
---
+++ bot/main.py
@@ -0,0 +1,24 @@
+import aiogram
+from dotenv import load_dotenv
+import os
+from aiogram import Bot,Dispatcher
+import asyncio
+from handlers import router
+import sys
+from config import PROJECT_ROOT
+
+load_dotenv()
+
+bot = Bot(os.getenv("TOKEN"))
+dp = Dispatcher()
+
+
+async def main():
+    dp.include_router(router)
+    dp.start_polling(bot)
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main)
+    except KeyboardInterrupt:
+        print("DONE")
