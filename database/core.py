945e7e1e0400439a8e0df8fc5b27ed57cda95c07
Wizer27 <svd2yar@gmail.com>
2026-02-15 22:46:15 +0300

    /

--- database/core.py
+++ database/core.py
@@ -1,2 +1,10 @@
 from sqlalchemy import select
 from models import metadata_obj,main_table
+from typing import Optional,List 
+import uuid
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
+#from chats_models import metadata_obj,chats_table
+import asyncio
